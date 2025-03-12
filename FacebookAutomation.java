6e304cecce2634bf6950e1a27524e2c1cd9a3bbf
kayalvizhi1229 <74707928+kayalvizhi1229@users.noreply.github.com>
2025-03-12 23:21:13 +0530

    FacebookAutomation.java

---
+++ FacebookAutomation.java
@@ -0,0 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FacebookAutomation {
+    private static final Logger log = LogManager.getLogger(FacebookAutomation.class);
+
+    public void login() {
+        log.info("Attempting to login to Facebook.");
+    }
+}
